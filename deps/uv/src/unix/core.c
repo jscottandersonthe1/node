d73e3da8d1ae1e9a12145324bb59d5a4cdd48990
Matthew Brandyberry <mbrandy@us.ibm.com>
2014-03-04 13:06:49 -0600

    AIX support for deps/uv.

--- deps/uv/src/unix/core.c
+++ deps/uv/src/unix/core.c
@@ -60,6 +60,10 @@
 # include <sys/wait.h>
 #endif
 
+#ifdef _AIX
+#include <sys/ioctl.h>
+#endif
+
 static void uv__run_pending(uv_loop_t* loop);
 
 /* Verify that uv_buf_t is ABI-compatible with struct iovec. */
@@ -434,7 +438,8 @@ int uv__close(int fd) {
 }
 
 
-#if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__) || \
+    defined(_AIX)
 
 int uv__nonblock(int fd, int set) {
   int r;
