909b74063b83dff970d0105fd496b239b00dba3f
Andrew Low <andrew_low@ca.ibm.com>
2014-04-22 13:56:29 -0400

    Enable OpenSSL on s390

--- deps/openssl/config/opensslconf.h
+++ deps/openssl/config/opensslconf.h
@@ -205,9 +205,9 @@
  /* If this is set to 'unsigned int' on a DEC Alpha, this gives about a
   * %20 speed up (longs are 8 bytes, int's are 4). */
 # undef DES_LONG
-# if defined(_M_X64) || defined(__x86_64__) || defined(__arm__) || defined(__mips__) || defined(__powerpc64__)
+# if defined(_M_X64) || defined(__x86_64__) || defined(__arm__) || defined(__mips__) || defined(__powerpc64__) || defined(__s390x)
 #  define DES_LONG unsigned int
-# elif defined(_M_IX86) || defined(__i386__) || defined(__powerpc__) || defined(_ARCH_PPC)
+# elif defined(_M_IX86) || defined(__i386__) || defined(__powerpc__) || defined(_ARCH_PPC) || defined(__s390__)
 #  define DES_LONG unsigned long
 # endif
 #endif
@@ -233,9 +233,9 @@
 # undef EIGHT_BIT
 # if (defined(_M_X64) || defined(__x86_64__)) && defined(_WIN32)
 #  define SIXTY_FOUR_BIT
-# elif (defined(_M_X64) || defined(__x86_64__) || defined(__powerpc64__)) && !defined(_WIN32)
+# elif (defined(_M_X64) || defined(__x86_64__) || defined(__powerpc64__)) || defined(__s390x) && !defined(_WIN32)
 #  define SIXTY_FOUR_BIT_LONG
-# elif defined(_M_IX86) || defined(__i386__) || defined(__arm__) || defined(__mips__) || defined(__powerpc__) || defined(_ARCH_PPC)
+# elif defined(_M_IX86) || defined(__i386__) || defined(__arm__) || defined(__mips__) || defined(__powerpc__) || defined(_ARCH_PPC) || defined(__s390__)
 #  define THIRTY_TWO_BIT
 # endif
 #endif
