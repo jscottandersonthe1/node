4e5152e2eefc6ddb748688e2825c665e0e90ce9b
Andrew Low <Andrew_Low@ca.ibm.com>
2014-05-06 12:08:43 -0400

    Update v8z to tag export-to-node-2014-05-06

--- deps/v8z/src/s390/lithium-gap-resolver-s390.cc
+++ deps/v8z/src/s390/lithium-gap-resolver-s390.cc
@@ -36,7 +36,7 @@
 namespace v8 {
 namespace internal {
 
-static const Register kSavedValueRegister = { 11 };
+static const Register kSavedValueRegister = { 1 };
 
 LGapResolver::LGapResolver(LCodeGen* owner)
     : cgen_(owner), moves_(32, owner->zone()), root_index_(0), in_cycle_(false),
@@ -281,7 +281,7 @@ void LGapResolver::EmitMove(int index) {
         // kSavedDoubleValueRegister was used to break the cycle,
         // but kSavedValueRegister is free.
 #if V8_TARGET_ARCH_S390X
-        __ ld(kSavedValueRegister, source_operand);
+        __ lg(kSavedValueRegister, source_operand);
         __ stg(kSavedValueRegister, destination_operand);
 #else
         MemOperand source_high_operand =
